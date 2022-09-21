0120d84e7a9f201ef0c8d9c3af3129a69dcc1788
newwangzhicheng <newwangzhicheng@users.noreply.github.com>
2022-09-21 10:24:56 +0800

    重新实现

--- instgram.js
+++ instgram.js
@@ -19,15 +19,125 @@
 (function () {
     'use strict';
 
+    const BTN_TAG = 'js-btn';
+    const CUR_SVG = `
+        <svg style="margin-top: 5px" t="1663695131319" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3070" width="32" height="32"><path d="M249.898667 85.333333h336.213333a64 64 0 0 1 41.301333 15.146667l203.264 171.904A64 64 0 0 1 853.333333 321.237333v535.637334c0 76.373333-0.853333 81.792-79.232 81.792H249.898667C171.52 938.666667 170.666667 933.290667 170.666667 856.874667V167.125333C170.666667 90.752 171.52 85.333333 249.898667 85.333333z" fill="#1296db" opacity=".3" p-id="3071"></path><path d="M635.221333 504.746667H551.68V419.157333a21.333333 21.333333 0 0 0-21.333333-21.333333h-43.648a21.333333 21.333333 0 0 0-21.333334 21.333333v85.674667H381.866667a21.333333 21.333333 0 0 0-16.256 35.114667l126.634666 149.546666a21.333333 21.333333 0 0 0 32.554667 0l126.634667-149.546666a21.333333 21.333333 0 0 0-16.256-35.114667z" fill="#1296db" p-id="3072"></path></svg>
+    `;
+    const COL_SVG = `
+        <svg style="margin-top: 5px" t="1663695241690" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3745" width="32" height="32"><path d="M149.333333 896h725.333334a64 64 0 0 0 64-64v-469.333333A64 64 0 0 0 874.666667 298.666667H426.666667L317.44 189.44A64 64 0 0 0 272.128 170.666667H149.333333A64 64 0 0 0 85.333333 234.666667v597.333333A64 64 0 0 0 149.333333 896z" fill="#1296db" opacity=".3" p-id="3746"></path><path d="M635.221333 547.413333H551.68v-85.632a21.333333 21.333333 0 0 0-21.333333-21.333333h-43.648a21.333333 21.333333 0 0 0-21.333334 21.333333v85.674667H381.866667a21.333333 21.333333 0 0 0-16.256 35.114667l126.634666 149.546666a21.333333 21.333333 0 0 0 32.554667 0l126.634667-149.546666a21.333333 21.333333 0 0 0-16.256-35.114667z" fill="#1296db" p-id="3747"></path></svg>
+    `;
+
+    const intervalTimer = setInterval(() => {
+        // 帖子
+        const posts = document.querySelectorAll('article[role="presentation"]');
+        for (let i = 0; i < posts.length; i++) {
+            const post = posts[i];
+            // 判断是否已经添加下载按钮
+            if (!hasDownloadBtn(post)) {
+                // 添加下载当前资源按钮
+                addDownloadCurrentBtn(post);
+                // 判断帖子是否包含多个元素,  添加下载合集按钮
+                if (hasMultipleAttachments(post)) {
+                    addDownloadCollectionBtn(post);
+                }
+            }
+        }
+
+    }, 500)
+
+    // 判断是否已经有下载按钮
+    function hasDownloadBtn(el) {
+        return el.querySelector(`[${BTN_TAG}]`) != null;
+    }
+
+    // 添加下载当前资源按钮
+    function addDownloadCurrentBtn(el) {
+        const container = el.querySelector('section');
+
+        const btn = getSvgBtn(CUR_SVG);
+
+        addBtn(btn, container, downloadCurrent);
+    }
+
+    // 添加下载合集按钮
+    function addDownloadCollectionBtn(el) {
+        const container = el.querySelector('section');
+        const btn = getSvgBtn(COL_SVG);
+
+        addBtn(btn, container, downloadCollection);
+    }
+
+    // 判断是否含多个资源
+    function hasMultipleAttachments(el) {
+        const attachments = el.querySelectorAll('li[style^="transform"]');
+        console.log(attachments)
+        return attachments.length > 0;
+    }
+
+    // 将svg字符串转化成元素
+    function getSvgBtn(svg){
+        const span = document.createElement('span');
+        span.setAttribute(BTN_TAG, '');
+        span.innerHTML = svg;
+        return span;
+    }
+
+    // 将按钮添加到页面中
+    function addBtn(btn, container, handler) {
+        container.append(btn);
+        $(btn).click(handler);
+    }
+
+    function downloadCurrent() {
+        console.log('download current');
+    }
+
+    function downloadCollection() {
+        console.log('download collection');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 判断点击的是否是三个小点的更多按钮
+    function isMoreOptionsEl(target){
+        if(target && (target.getAttribute('aria-label') == '更多选项' || target.querySelector('[aria-label="更多选项"]'))){
+            return true;
+        }
+        return false;
+    }
+
+    // 根据子元素获取当前post元素（article）
+    function getPostEl(childEl) {
+        return childEl.closest('article');
+    }
+    // 根据post元素（artile）判断当前post是否有多个元素
+    function isMultiPost(postEl) {
+        // 返回按钮
+        const hasReturnBtn = postEl.querySelector('[aria-label="返回"]') != null;
+        // 下一步按钮
+        const hasNextBtn = postEl.querySelector('[aria-label="下一步"]') != null;;
+        return hasReturnBtn || hasNextBtn;
+    }
     // Your code here...
-    /** 更多选项列表的选择器 */
-    const selectionListSelector = 'div.RnEpo.Yx5HN > div > div > div > div';
-    /** 当前post下的用户名称选择器 */
-    const usernameASelector = 'header.Ppjfr  a.ZIAjV';
+    /** 更多选项的选择器 */
+    const moreDialogSelector = '[role="dialog"]';
+    /** 当前post下(article标签)的用户名称选择器, 获取用户的名称使用textContent */
+    const usernameASelector = 'header>div:nth-child(2)>div:first-child';
     /** 复制图片按钮选择器 */
-    const copyURLSelector = `${selectionListSelector} > button:nth-last-child(3)`;
+    const copyURLSelector = `${moreDialogSelector} > button:nth-last-child(3)`;
     /** 打开帖子按钮选择器 */
-    const openPostSelector = `${selectionListSelector} > button:nth-last-child(2)`;
+    const openPostSelector = `${moreDialogSelector} > button:nth-last-child(2)`;
     /** 单个下载按钮 */
     const singleDownloadBtn = '<button jaywangdownload="single" class="aOOlW" tabindex="0" style="color: #58c322">下载</button>';
     /** 批量下载图片按钮 */
@@ -72,7 +182,7 @@
             if (isPrivateUser() && withoutOpenPostBtn()) {
                 //console.log(`location`, location)
             } else {
-                $(`${selectionListSelector} > button:nth-last-child(${getOpenPostLastLocation()})`).click();
+                $(`${moreDialogSelector} > button:nth-last-child(${getOpenPostLastLocation()})`).click();
             }
             initSrc = location.href;
             const data = await fetchPostInformation(initSrc);
@@ -81,44 +191,45 @@
         }
 
         /** 点击三个小点更多按钮 */
-        if (el.target.closest('button.wpO6b')) {
+        if (isMoreOptionsEl(el.target)) {
             /** 获取当前post */
-            currentPost = el.target.closest('article');
-            /** 容纳点点点的容器 */
-            const container = currentPost.querySelector('._97aPb');
-            const dots = currentPost.querySelector('._3eoV-');
-            const isMulti = isMultiplePost(container);
-            currentIndex = isMulti ? getPostIndex(dots) : 0;
+            currentPost = getPostEl(el.target);
+            /** 根据返回 下一个按钮是否是多图的post */
+            const isMulti = isMultiPost(currentPost);
+            // TODO
+            // currentIndex = isMulti ? getPostIndex(dots) : 0;
+            console.log('Post has multiple photos or images: ', isMulti)
         }
     });
-    /**  */
 
     /** DOM变动的回调函数 */
     const callback = function (mutationRecord) {
+        console.log()
         for (const record of mutationRecord) {
             const nodeList = record.addedNodes;
-            if (nodeList.length === 1 && isMoreOptionButton(nodeList[0])) {
-                $(selectionListSelector).prepend(singleDownloadBtn);
-                $(selectionListSelector).prepend(batchDownloadBtn);
+            console.log(nodeList)
+            if (false && nodeList.length === 1 && isMoreOptionsEl(nodeList[0])) {
+                $(moreDialogSelector).prepend(singleDownloadBtn);
+                $(moreDialogSelector).prepend(batchDownloadBtn);
                 return;
             }
         }
     };
     /** 检测DOM变动 */
     const observer = new MutationObserver(callback);
-    observer.observe(document.body, {
-        childList: true,
-        subtree: false
-    });
+   // observer.observe(document.body, {
+   //     childList: true,
+  //      subtree: false
+  //  });
 
     /**
      * 判断是否是更多选项按钮
      * @param {Node} node
      * @return {boolean}
      */
-    function isMoreOptionButton(node) {
-        return node.querySelector('.mt3GC');
-    }
+    //function isMoreOptionButton(node) {
+      //  return node.querySelector('.mt3GC');
+    //}
 
     /**
      * 获取Post信息
@@ -269,15 +380,6 @@
     }
 
     /**
-     * 判断post图片/视频数量是多个还是单个
-     * @param {Element} el container
-     * @return {boolean}
-     */
-    function isMultiplePost(el) {
-        return el.querySelector('._3eoV-') !== null;
-    }
-
-    /**
      * 格式化的日期
      * @returns {string}
      */
@@ -291,7 +393,7 @@
      * @returns {boolean}
      */
     function isPrivateUser() {
-        const nodeList = document.querySelector(selectionListSelector).querySelectorAll('.HoLwm');
+        const nodeList = document.querySelector(moreDialogSelector).querySelectorAll('.HoLwm');
         return nodeList.length <= 3;
     }
 
@@ -300,7 +402,7 @@
      * @returns {boolean}
      */
     function withoutOpenPostBtn() {
-        const nodeList = document.querySelector(selectionListSelector).querySelectorAll('.HoLwm');
+        const nodeList = document.querySelector(moreDialogSelector).querySelectorAll('.HoLwm');
         return nodeList.length === 1;
     }
 
@@ -309,7 +411,7 @@
      * @returns {number}
      */
     function getOpenPostLastLocation() {
-        const nodeList = document.querySelector(selectionListSelector).querySelectorAll('.HoLwm');
+        const nodeList = document.querySelector(moreDialogSelector).querySelectorAll('.HoLwm');
         return nodeList.length;
     }
 
